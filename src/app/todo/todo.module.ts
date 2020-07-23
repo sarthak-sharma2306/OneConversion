7f510592d4fa52a1306ce8178d89e2b497b24e97
Sarthak Sharma <sarthak.sharma2306@gmail.com>
2020-07-23 13:51:51 +0530

    created "completed-tasks" component in todo module

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CompletedTasksComponent } from './completed-tasks/completed-tasks.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CompletedTasksComponent],
   imports: [
     CommonModule
   ]
