90e83450ac2542b8bff5b63de3ad8cae56c6e8ba
Sarthak Sharma <sarthak.sharma2306@gmail.com>
2020-07-24 10:44:41 +0530

    routing outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'learning', component: TasksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
